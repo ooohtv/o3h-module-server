6bb10ce2bc93471fa3ea529d5f6bc52f5b9873aa
Roger Braunstein <roger@oooh.tv>
2020-08-13 10:56:55 -0700

    Creates a dead-simple way for module developers to set up a local server

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # o3h-module-server
-A simple http server for static Oooh module content &amp; the Oooh API
+A simple http server for static Oooh module content &amp; the Oooh API. Serves from your local directory and proxies /api/ URLs to https://oooh.tv/api/.
+
+## To install
+
+```npm install -g git:github.com/roger-o3h/o3h-module-server```
+
+## To run
+
+Just run
+
+```o3h-module-server```
+
+in the directory you would like to serve from.
+
+## Options
+
+`--port <int>` or `-p <int>` — The port to serve on, defaults to 3000.
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+let port = 3000;
+for (let i = 0; i < process.argv.length; i++) {
+    const arg = process.argv[i];
+    if ((arg === '--port' || arg === '-p') && i < process.argv.length - 1) {
+        port = parseInt(process.argv[++i]);
+    }
+}
+
+const app = express();
+app.use(express.static('.'));
+app.use('/api/', createProxyMiddleware({
+        target: 'http://www.oooh.tv/',
+        changeOrigin: true,
+        autoRewrite: true,
+        followRedirects: true,
+        onProxyRes: (proxyRes, req, res) => {
+            if (req.url.endsWith('/o3h.js')) {
+                res.setHeader('Content-Type', 'text/javascript');
+                res.write('const LOCAL_DEVELOPMENT = true; //');
+            }
+        }
+    }
+));
+app.listen(port, function () {
+    console.log(`Listening on http://localhost:${port}/`);
+});
