c156bc3f8fb600ffad22b3474894869a7cc5d575
Roger Braunstein <roger@oooh.tv>
2020-08-21 16:34:43 -0700

    Forces case sensitivity by checking exact file case in middleware

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// commandline options
+let root = '.';
 let port = 3000;
 let useApiProxy = true;
+let caseSensitive = true;
 for (let i = 0; i < process.argv.length; i++) {
     const arg = process.argv[i];
     if ((arg === '--port' || arg === '-p') && i < process.argv.length - 1) {
         port = parseInt(process.argv[++i]);
     }
-    if (arg === '--no-proxy-api'|| arg === '-A') {
+    if ((arg === '--root' || arg === '-r') && i < process.argv.length - 1) {
+        root = process.argv[++i];
+    }
+    if (arg === '--no-proxy-api' || arg === '-A') {
         useApiProxy = false;
+    }
+    if (arg === '--no-case-sensitive' || arg === '-C') {
+        caseSensitive = false;
     }
 }
 
+root = path.resolve(root);
 const app = express();
-app.use(express.static('.'));
 if (useApiProxy) {
     app.use('/api/', createProxyMiddleware({
             target: 'http://www.oooh.tv/',
@@ -30,7 +41,26 @@ if (useApiProxy) {
         }
     ));
 }
+if (caseSensitive) {
+    app.use((req, res, next) => {
+        const absPath = path.resolve(root, './' + req.path);
+        if (!fs.existsSync(absPath)) {
+            // if the file does not exist, handle the 404 normally
+            next();
+            return;
+        }
+        const pathParts = path.parse(absPath);
+        const isExactCase = fs.readdirSync(pathParts.dir).some(file => file === pathParts.base);
+        if (isExactCase) {
+            next();
+        } else {
+            res.status(404)
+               .send(`Cannot ${req.method} ${req.path}, please check the cAsE Of YouR FILenAMe.`);
+        }
+    });
+}
+app.use(express.static(root));
 app.get('/o3h.dev.txt', (req, res) => res.send('OK'));
 app.listen(port, function () {
-    console.log(`Listening on http://localhost:${port}/`);
+    console.log(`Serving ${root} on http://localhost:${port}/`);
 });
