76c7bb7e24288be600523739e9f85931a3a3481d
Roger Braunstein <roger@oooh.tv>
2020-08-21 16:05:23 -0700

    Adds option to serve our own API files

--- index.js
+++ index.js
@@ -2,28 +2,34 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 let port = 3000;
+let useApiProxy = true;
 for (let i = 0; i < process.argv.length; i++) {
     const arg = process.argv[i];
     if ((arg === '--port' || arg === '-p') && i < process.argv.length - 1) {
         port = parseInt(process.argv[++i]);
     }
+    if (arg === '--no-proxy-api'|| arg === '-A') {
+        useApiProxy = false;
+    }
 }
 
 const app = express();
 app.use(express.static('.'));
-app.use('/api/', createProxyMiddleware({
-        target: 'http://www.oooh.tv/',
-        changeOrigin: true,
-        autoRewrite: true,
-        followRedirects: true,
-        onProxyRes: (proxyRes, req, res) => {
-            if (req.url.endsWith('/o3h.js')) {
-                res.setHeader('Content-Type', 'text/javascript');
-                res.write('const LOCAL_DEVELOPMENT = true; //');
+if (useApiProxy) {
+    app.use('/api/', createProxyMiddleware({
+            target: 'http://www.oooh.tv/',
+            changeOrigin: true,
+            autoRewrite: true,
+            followRedirects: true,
+            onProxyRes: (proxyRes, req, res) => {
+                if (req.url.endsWith('/o3h.js')) {
+                    res.setHeader('Content-Type', 'text/javascript');
+                    res.write('const LOCAL_DEVELOPMENT = true; //');
+                }
             }
         }
-    }
-));
+    ));
+}
 app.get('/o3h.dev.txt', (req, res) => res.send('OK'));
 app.listen(port, function () {
     console.log(`Listening on http://localhost:${port}/`);
